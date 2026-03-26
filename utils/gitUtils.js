75c101a8d819bb282247fbf3ed27d003b101c5ba
ShinonMaki <vaneshinon@gmail.com>
2026-03-26 12:38:39 +0100

    Create gitUtils.js

---
+++ utils/gitUtils.js
@@ -0,0 +1,23 @@
+const { exec } = require("child_process");
+
+function gitCommitAndPush(message, callback) {
+  exec(
+    `cd /root/Hero-Dex && git add heroes.json images pdf && git commit -m "${message}" && git push`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error("Git push error:", err);
+        console.error(stderr);
+        if (callback) callback(false, stderr);
+        return;
+      }
+
+      console.log("Git push success:");
+      console.log(stdout);
+      if (callback) callback(true, stdout);
+    }
+  );
+}
+
+module.exports = {
+  gitCommitAndPush
+};
