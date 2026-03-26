24ae1712b648fc8fe74f84739c94538675bf2588
ShinonMaki <vaneshinon@gmail.com>
2026-03-26 12:35:39 +0100

    constants.js

---
+++ config/constants.js
@@ -0,0 +1,16 @@
+const PREFIX = ".";
+
+const typeColors = {
+  truth: 0x2ecc71,
+  order: 0x3498db,
+  combat: 0xe74c3c,
+  light: 0xf1c40f,
+  dark: 0x8e44ad,
+  chaos: 0x7f8c8d,
+  default: 0x95a5a6
+};
+
+module.exports = {
+  PREFIX,
+  typeColors
+};
