ecd3d47a5a281c738246595b4b10ee9cb85a0bf8
ShinonMaki <vaneshinon@gmail.com>
2026-03-26 12:36:52 +0100

    Create heroSessions.js

---
+++ sessions/heroSessions.js
@@ -0,0 +1,9 @@
+const heroCreationSessions = new Map();
+const heroDeleteSessions = new Map();
+const heroEditSessions = new Map();
+
+module.exports = {
+  heroCreationSessions,
+  heroDeleteSessions,
+  heroEditSessions
+};
