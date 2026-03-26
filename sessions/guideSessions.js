e03817ffc8745506b9565d59a2d9e20dcdc5e2e2
ShinonMaki <vaneshinon@gmail.com>
2026-03-27 00:10:21 +0100

    Update guideSessions.js

--- sessions/guideSessions.js
+++ sessions/guideSessions.js
@@ -2,10 +2,12 @@ const guideAddSessions = new Map();
 const guideEditSessions = new Map();
 const guideDeleteSessions = new Map();
 const guideViewSessions = new Map();
+const guideRenameCategorySessions = new Map();
 
 module.exports = {
   guideAddSessions,
   guideEditSessions,
   guideDeleteSessions,
-  guideViewSessions
+  guideViewSessions,
+  guideRenameCategorySessions
 };
